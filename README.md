b1560d1c48c5cbe1286d6a891694cb8b4cee6200
Shashi-Anand <shashianand2600@gmail.com>
2025-01-27 20:26:59 +0530

    youtube project create

---
+++ README.md
@@ -0,0 +1,6 @@
+# Shashi-Youtube
+
+- Create a React + Vite application
+- Remove unnecessary code and create hello world app
+
+
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="underline text-2xl">Hello World</h1>
+    </>
+  )
+}
+
+export default App
