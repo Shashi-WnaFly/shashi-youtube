12e6d636fb34b3a3b2458584a5c36382c38dbb6d
Shashi-Anand <shashianand2600@gmail.com>
2025-01-31 21:51:18 +0530

    trying to fetch data from youtube api

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
 import VideoCard from "./VideoCard";
+import { YOUTUBE_VIDEO_API } from "../utils/constant";
 
 const VideoContainer = () => {
+
+  const getData = async function() {
+    const data = await fetch(YOUTUBE_VIDEO_API);
+    const json = await data.json();
+    console.log(json);
+  }
+  
+  useEffect( () => {
+    getData();
+  }, []);
   return (
-    <>
+    <div>
       <VideoCard />
-    </>
+    </div>
   );
 };
 
