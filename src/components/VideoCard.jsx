cb0a9ef1d26c17adfb8ec19ed8d1e9867723941c
Shashi-Anand <shashianand2600@gmail.com>
2025-02-03 20:51:18 +0530

    creating video card component

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,19 @@
+/* eslint-disable react/prop-types */
+import PropTypes from "prop-types";
 
-const VideoCard = () => {
+const VideoCard = (props) => {
+  const { items } = props;
+  if(!items) return null;
+
   return (
-    <div>VideoCard</div>
+    <div className=" rounded-xl">
+      <img className="rounded-xl" src={items?.snippet?.thumbnails?.medium?.url} />
+    </div>
   )
 }
+
+VideoCard.prototype = {
+  items : PropTypes.object.isRequired,
+};
 
 export default VideoCard;
--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,13 +1,15 @@
+import { useSelector } from "react-redux";
 import usePopularVideo from "../Hooks/usePopularVideo";
 import VideoCard from "./VideoCard";
 
 const VideoContainer = () => {
 
   usePopularVideo();
-  
+  const popularVideos =  useSelector((store) => store.app.popularYoutubeVideos);
+  console.log(popularVideos)
   return (
-    <div>
-      <VideoCard />
+    <div className="grid grid-cols-4 gap-4">
+      {popularVideos?.items?.map((item) => <VideoCard key={item.id} items = {item}/>)}
     </div>
   );
 };
