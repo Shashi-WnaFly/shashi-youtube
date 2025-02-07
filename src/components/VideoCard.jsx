a9cf0a5975791685c8c11544b825e49b38d309a0
Shashi-Anand <shashianand2600@gmail.com>
2025-02-07 22:19:29 +0530

    ui update in tvideocard and container

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,16 +5,26 @@ const VideoCard = ({ items }) => {
 
   const { channelTitle, localized, thumbnails } = items.snippet;
   const { statistics } = items;
+  
+  let view = parseInt(statistics.viewCount);
+  if(view > 100000){
+    view = Math.round(view/100000);
+    view = view + "M";
+  } 
+  else if( view > 1000){
+    view = Math.round(view/1000);
+    view = view + "K";
+  }
 
   return (
     <div className="flex flex-col shadow-2xl">
       <div className=" rounded-xl flex justify-center">
         <img className="rounded-xl w-full" src={thumbnails?.medium?.url} />
       </div>
-      <div>
-        <div className=" font-semibold">{localized.title}</div>
-        <div>{channelTitle}</div>
-        <div>{statistics.viewCount} views</div>
+      <div className="p-2 pl-4">
+        <div className=" font-semibold text-nowrap overflow-hidden overflow-ellipsis">{localized.title}</div>
+        <div className="text-sm ">{channelTitle}</div>
+        <div className="text-sm ">{view} views</div>
       </div>
     </div>
   );
