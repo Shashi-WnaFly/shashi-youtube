d4c018cd69878bd8d2e5757550618a0571ce93ba
Shashi-Anand <shashianand2600@gmail.com>
2025-02-09 20:59:43 +0530

    implementing of routing of the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,34 @@ import { Provider } from "react-redux";
 import Body from "./components/Body";
 import Header from "./components/Header";
 import store from "./utils/store";
+import WatchPage from "./components/WatchPage";
+import MainContainer from "./components/MainContainer"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/watch",
+        element: <WatchPage />
+      },
+      {
+        path: "/",
+        element: <MainContainer />
+      }
+      
+    ]
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <div className="relative">
         <Header />
-        <Body />
+        <RouterProvider router={appRouter} />
       </div>
     </Provider>
   );
