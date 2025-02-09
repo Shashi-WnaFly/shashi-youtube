50d19320aaa51a5ad8283467d6e3b70070010f0a
Shashi-Anand <shashianand2600@gmail.com>
2025-02-09 22:02:00 +0530

    stuck into the video id

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -1,17 +1,30 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
+  const {searchParams} = useSearchParams();
+  console.log(searchParams.get("v"))
 
-  useEffect( () => {
+  useEffect(() => {
     dispatch(closeMenu());
   }, []);
 
   return (
-    <div>WatchPage</div>
-  )
-}
+    <div>
+      <iframe
+        width="853"
+        height="480"
+        src={"https://www.youtube.com/embed/"+videoId}
+        title="39  Evaluate Expression to True  Boolean Parenthesization Recursive"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
 
 export default WatchPage;
