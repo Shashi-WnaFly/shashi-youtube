c05199e66eb375a3abaf91b987dc005130cdc500
Shashi-Anand <shashianand2600@gmail.com>
2025-02-12 19:27:38 +0530

    trying to fix buttonlist

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,10 +1,19 @@
 import { buttons } from "../utils/constant";
 
-const btnList = buttons.map((str, i) => <button className=" w-[84] rounded-2xl py-2 px-4 font-medium bg-[#ecebeb]" key={i}>{str}</button>);
+const btnList = buttons.map((str, i) => (
+  <button
+    className="rounded-lg py-2 px-4 font-medium cursor-pointer bg-[#ecebeb]"
+    key={i}
+  >
+    {str}
+  </button>
+));
 const ButtonList = () => {
   return (
-    <div className="flex gap-4 text-sm pl-10 bg-white ">{btnList}</div>
-  )
-}
+    <div className="pl-4 max-w-7xl overflow-x-hidden ">
+      <div className="text-sm text-nowrap flex gap-2 p-3">{btnList}</div>
+    </div>
+  );
+};
 
 export default ButtonList;
